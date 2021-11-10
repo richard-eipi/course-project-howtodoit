fee2441c2396cf2140b7e3e696f23faff75da588
Richard Soma <richard.soma@mail.utoronto.ca>
2021-11-10 16:25:15 -0500

    setting up use case methods

--- src/main/java/usecasesControllers/QueryUseCases.java
+++ src/main/java/usecasesControllers/QueryUseCases.java
@@ -6,4 +6,29 @@ public class QueryUseCases implements QueryInputBoundary {
     public QueryUseCases(UserList userList) {
         this.userList = userList;
     }
+
+    @Override
+    public String viewTeams(String username) {
+        return null;
+    }
+
+    @Override
+    public String viewMemsIn(String username, String teamName) {
+        return null;
+    }
+
+    @Override
+    public String viewProjs(String username) {
+        return null;
+    }
+
+    @Override
+    public String viewTasks(String username) {
+        return null;
+    }
+
+    @Override
+    public String viewTasksInProj(String username, String projName) {
+        return null;
+    }
 }
--- src/main/java/usecasesControllers/TeamController.java
+++ src/main/java/usecasesControllers/TeamController.java
@@ -15,4 +15,58 @@ public class TeamController {
     public void setInputBoundary(TeamInputBoundary inputBoundary) {
         this.inputBoundary = inputBoundary;
     }
+
+    public String newTeam(String username, String teamName) throws Exception {
+        boolean result = this.inputBoundary.newTeam(username, teamName);
+        if (result) {
+            return "New team created successfully.";
+        } else {
+            throw new Exception("Failure to create new team.");
+        }
+    }
+
+    public String delTeam(String username, String teamName) throws Exception {
+        boolean result = this.inputBoundary.delTeam(username, teamName);
+        if (result) {
+            return "Team deleted successfully.";
+        } else {
+            throw new Exception("Failure to delete team.");
+        }
+    }
+
+    public String modTeam(String username, String name1, String name2) throws Exception {
+        boolean result = this.inputBoundary.modTeam(username, name1, name2);
+        if (result) {
+            return "Team renamed successfully.";
+        } else {
+            throw new Exception("Failure to rename team.");
+        }
+    }
+
+    public String addMem(String username, String teamName, String memName) throws Exception {
+        boolean result = this.inputBoundary.addMem(username, teamName, memName);
+        if (result) {
+            return "Member added successfully.";
+        } else {
+            throw new Exception("Failure to add member.");
+        }
+    }
+
+    public String delMem(String username, String teamName, String memName) throws Exception {
+        boolean result = this.inputBoundary.delMem(username, teamName, memName);
+        if (result) {
+            return "Member deleted successfully.";
+        } else {
+            throw new Exception("Failure to delete member.");
+        }
+    }
+
+    public String addAdmin(String username, String teamName, String memName) throws Exception {
+        boolean result = this.inputBoundary.addAdmin(username, teamName, memName);
+        if (result) {
+            return "Admin added successfully.";
+        } else {
+            throw new Exception("Failure to add admin.");
+        }
+    }
 }
