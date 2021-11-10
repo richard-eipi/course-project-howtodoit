7e411711d5419d98cb169ffdc26c835a318678aa
Richard Soma <richard.soma@mail.utoronto.ca>
2021-11-10 16:25:15 -0500

    setting up use case methods

--- src/main/java/usecasesControllers/LoginRegisterInputBoundary.java
+++ src/main/java/usecasesControllers/LoginRegisterInputBoundary.java
@@ -1,7 +1,19 @@
 package usecasesControllers;
 
 public interface LoginRegisterInputBoundary {
+    /**
+     * Lets the user log in.
+     * @param username user's username
+     * @param password user's password
+     * @return LoginResult indicating whether success, failure, or no such user
+     */
     LoginRegisterUseCases.LoginResult login(String username, String password);
 
+    /**
+     * Lets the user register for a new account.
+     * @param username user's username
+     * @param password user's password
+     * @return RegisterResult indicating whether success, failure, or no such user
+     */
     LoginRegisterUseCases.RegisterResult register(String username, String password);
 }
--- src/main/java/usecasesControllers/TaskUseCases.java
+++ src/main/java/usecasesControllers/TaskUseCases.java
@@ -6,4 +6,39 @@ public class TaskUseCases implements TaskInputBoundary {
     public TaskUseCases(UserList userList) {
         this.userList = userList;
     }
+
+    @Override
+    public boolean newTask(String username, String taskName, String dueDate, String projName) {
+        return false;
+    }
+
+    @Override
+    public boolean completeTask(String username, String taskName) {
+        return false;
+    }
+
+    @Override
+    public boolean star(String username, String taskName) {
+        return false;
+    }
+
+    @Override
+    public boolean unstar(String username, String taskName) {
+        return false;
+    }
+
+    @Override
+    public boolean rename(String username, String name1, String name2) {
+        return false;
+    }
+
+    @Override
+    public boolean retime(String username, String taskName, String dueDate) {
+        return false;
+    }
+
+    @Override
+    public boolean redesc(String username, String taskName, String desc) {
+        return false;
+    }
 }
