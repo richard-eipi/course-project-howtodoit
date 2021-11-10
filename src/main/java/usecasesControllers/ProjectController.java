da77b298e9702949e002ee59d6ddd123dbffac66
Richard Soma <richard.soma@mail.utoronto.ca>
2021-11-10 17:38:57 -0500

    adding javadoc and implementing controllers

--- src/main/java/usecasesControllers/ProjectController.java
+++ src/main/java/usecasesControllers/ProjectController.java
@@ -16,15 +16,30 @@ public class ProjectController {
         this.inputBoundary = inputBoundary;
     }
 
+    /**
+     * Create a new project.
+     * @param username current username
+     * @param projName name of the new project you want to create
+     * @param teamName name of the team this project belongs to, leave as "" if personal project
+     * @return String indicating success
+     * @throws Exception failure to create a new project
+     */
     public String newProj(String username, String projName, String teamName) throws Exception {
         boolean result = this.inputBoundary.newProj(username, projName, teamName);
         if (result) {
             return "New project created successfully.";
         } else {
-            throw new Exception("Failure to create new project.");
+            throw new Exception("Failure to create a new project.");
         }
     }
 
+    /**
+     * Delete a project; if personal, move all its tasks to the "General" project, otherwise delete all tasks.
+     * @param username current username
+     * @param projName name of the project you want to delete
+     * @return String indicating success
+     * @throws Exception failure to delete project
+     */
     public String delProj(String username, String projName) throws Exception {
         boolean result = this.inputBoundary.delProj(username, projName);
         if (result) {
@@ -34,6 +49,14 @@ public class ProjectController {
         }
     }
 
+    /**
+     * Change the name of a project.
+     * @param username current username
+     * @param name1 name of the project you want to change
+     * @param name2 the new name
+     * @return String indicating success
+     * @throws Exception failure to rename project
+     */
     public String modProj(String username, String name1, String name2) throws Exception {
         boolean result = this.inputBoundary.modProj(username, name1, name2);
         if (result) {
