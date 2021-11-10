4aba7200b532d873c6e75e210aa71a807b327e9a
Richard Soma <richard.soma@mail.utoronto.ca>
2021-11-10 18:08:18 -0500

    cleaning up the commands a little bit

--- src/main/java/constants/Commands.java
+++ src/main/java/constants/Commands.java
@@ -10,20 +10,13 @@ import java.util.TreeSet;
  */
 public class Commands {
     public static final HashMap<String, Command> COMMANDS = new HashMap<>();
-    public static final TreeSet<String> NON_MUTATING_COMMANDS= new TreeSet<>();
 
     /**
      * This class loads all commands.
      */
     public static void loadCommands() {
-        COMMANDS.put("register", new Register());
-        COMMANDS.put("login", new Login());
-        COMMANDS.put("logout", new Logout());
-        COMMANDS.put("exit", new Exit());
         COMMANDS.put("regret", new Regret());
 
-        COMMANDS.put("allUsers", new AllUsers());
-        COMMANDS.put("allTeams", new AllTeams());
         COMMANDS.put("viewTeams", new ViewTeams());
         COMMANDS.put("viewMemsIn", new ViewMemsIn());
         COMMANDS.put("viewAdminsIn", new ViewAdminsIn());
@@ -35,15 +28,12 @@ public class Commands {
         COMMANDS.put("viewTasksInProj", new ViewTasksInProj());
 
         COMMANDS.put("newProj", new NewProj());
-        COMMANDS.put("newTeamProj", new NewTeamProj());
         COMMANDS.put("delProj", new DelProj());
         COMMANDS.put("modProj", new ModProj());
 
         COMMANDS.put("newTeam", new NewTeam());
         COMMANDS.put("delTeam", new DelTeam());
         COMMANDS.put("modTeam", new ModTeam());
-        COMMANDS.put("joinTeam", new JoinTeam());
-        COMMANDS.put("leaveTeam", new LeaveTeam());
         COMMANDS.put("addMem", new AddMem());
         COMMANDS.put("delMem", new DelMem());
         COMMANDS.put("addAdmin", new AddAdmin());
@@ -55,17 +45,5 @@ public class Commands {
         COMMANDS.put("rename", new Rename());
         COMMANDS.put("redesc", new Redesc());
         COMMANDS.put("retime", new Retime());
-        COMMANDS.put("reproj", new Reproj());
-
-        populateNonMutatingCommands();
-    }
-
-    private static void populateNonMutatingCommands() {
-        NON_MUTATING_COMMANDS.add("upcoming");
-        NON_MUTATING_COMMANDS.add("viewproj");
-        NON_MUTATING_COMMANDS.add("listproj");
-        NON_MUTATING_COMMANDS.add("viewlab");
-        NON_MUTATING_COMMANDS.add("listlab");
-        NON_MUTATING_COMMANDS.add("regret");
     }
 }
