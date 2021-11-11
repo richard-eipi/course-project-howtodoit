2d9404799fca3e1a75fe64a1389a33273c308222
Richard Soma <richard.soma@mail.utoronto.ca>
2021-11-11 14:34:19 -0500

    refactoring enums + implementing query

---
+++ src/main/java/constants/Enums.java
@@ -0,0 +1,7 @@
+package constants;
+
+public class Enums {
+    public enum LoginResult {SUCCESS, FAILURE, NO_SUCH_USER}
+
+    public enum RegisterResult {SUCCESS, FAILURE}
+}
--- src/main/java/entities/Project.java
+++ src/main/java/entities/Project.java
@@ -1,12 +1,17 @@
 package entities;
 
+import helpers.SortTasksChronologically;
+
 import java.io.Serializable;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+
 /**
  * This class represents a project.
  * A project stores a collection of tasks, but those tasks must be in exactly one project.
  */
-public class Project implements Serializable {
+public class Project implements Serializable, Iterable<Task> {
 
     private String name;
     private final HashMap<String, Task> tasks = new HashMap<>();
@@ -39,5 +44,21 @@ public class Project implements Serializable {
 
     public void delTask(Task task) {
         this.tasks.remove(task.getName());
+    }
+
+    @Override
+    public String toString() {
+        List<Task> sortedTasks = SortTasksChronologically.sortTasks(this.tasks); // Sort them
+        StringBuilder output = new StringBuilder("This project <" + this.name + "> contains the following tasks:\n");
+        for (Task task: sortedTasks) {
+            output.append(task.toString()).append('\n'); // Each line will be a task
+        }
+
+        return output.toString();
+    }
+
+    @Override
+    public Iterator<Task> iterator() {
+        return this.tasks.values().iterator();
     }
 }
